74dafd69f2d87689491fdb1c9a0f8859899447fd
Sam <samuel.waters@gmail.com>
2022-02-03 20:09:18 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const x = document.getElementById("main");
+x.remove();
+
+const element = document.createElement("h1");
+document.body.append(element);
+const newHeader = document.querySelector("h1");
+newHeader.textContent = "YOUR-NAME is the champion";
+newHeader.id = "victory";
+
+
